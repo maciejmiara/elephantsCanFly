af976daf97d41dea34947d6df416421e3cc02fc9
Kamil Racznski <k.raczynski94@gmail.com>
2013-05-10 14:31:42 +0200

    update and add script

---
+++ script/jQuery_srcipt.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	$('.lvl-info').hide();
+	
+	$('.lvl').on('mouseover', function(){
+		$(this).removeClass("color_anim_2")
+		$(this).addClass("color_anim_1");
+		$(this).children('.lvl-info').slideDown();
+	});
+	
+	$('.lvl').on('mouseleave', function(){
+		
+		$(this).removeClass("color_anim_1");
+		$(this).filter("[name=1]").addClass("color_anim_2");
+		$(this).children('.lvl-info').slideUp();
+		//$(this).removeClass("color_anim_2");
+	});
+	$('.lvl').on('click', function(){
+		var name = $(this).attr('name');
+		window.location = "/game.php?lvl="+name;
+		return false;
+	});
+	
+});
