c2ec6201580d65a950f2e1211b13924e9ea5e8b9
Kamil Racznski <k.raczynski94@gmail.com>
2013-05-14 11:37:34 +0200

    ze stosu

---
+++ css/newPHPClass.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of newPHPClass
+ *
+ * @author Kamil
+ */
+class newPHPClass {
+
+ protected $result = 34;
+}
+
+?>
